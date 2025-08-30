f4691baf1ea25ac65a60a28301f2fbaf973d3350
Daniel Macharia <ndungudanny444@gmail.com>
2025-08-31 00:14:42 +0300

    feat(UI design): finish up UI design. Use bootstrap for styles

--- src/app/features/student-list/student-list.ts
+++ src/app/features/student-list/student-list.ts
@@ -1,11 +1,22 @@
 import { DialogRef } from '@angular/cdk/dialog';
-import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MtxDialog, MtxDialogModule } from '@ng-matero/extensions/dialog';
 import { MtxGridColumn, MtxGridModule } from '@ng-matero/extensions/grid';
+import { MtxSelectModule } from '@ng-matero/extensions/select';
 
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MtxDatetimepickerModule } from '@ng-matero/extensions/datetimepicker';
+import { MtxNativeDatetimeModule } from '@ng-matero/extensions/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 interface StudentData{
   studentId: number,
@@ -22,16 +33,29 @@ interface StudentData{
 @Component({
   selector: 'app-student-list',
   standalone:true,
-  imports: [MatCardModule, 
+  imports: [
+    CommonModule,
+    MatCardModule, 
     MatIconModule, 
     MatButtonModule, 
     MtxGridModule,  
-    MtxDialogModule
+    MtxDialogModule,
+    MtxSelectModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
   ],
   templateUrl: './student-list.html',
   styleUrl: './student-list.css'
 })
-export class StudentList {
+export class StudentList implements OnInit{
+
+  private mtxDialog = inject(MtxDialog);
+  private formBuilder = inject(FormBuilder);
+
   @ViewChild('editStudentDialog') editStudentDialog!: TemplateRef<any>;
   @ViewChild('confirmDeleteStudentDialog') confirmDeleteStudentDialog!: TemplateRef<any>;
 
@@ -39,6 +63,27 @@ export class StudentList {
 
   studentDataBeingEdited: StudentData|null = null;
   editMode = false;
+  classOptions: string[] = [ 'Class 1', 'Class 2', 'Class 3'];
+
+  editStudentForm: FormGroup = this.formBuilder.group({
+    studentId: 0,
+      firstName: '',
+      lastName: '',
+      dob: [null],
+      class: '',
+      score: 0,
+      passportPhotoUrl: '',
+      frontIDPhotoUrl: '',
+      backIDPhotoUrl: ''
+  });
+
+  selectedPassportPhoto: File | null = null;
+  selectedFrontIDPhoto: File | null = null;
+  selectedBackIDPhoto: File | null = null;
+  
+  previewPassportPhotoUrl: string | ArrayBuffer | null = null;
+  previewFrontIDPhotoUrl: string | ArrayBuffer | null = null;
+  previewBackIDPhotoUrl: string | ArrayBuffer | null = null;
 
   columns: MtxGridColumn[] = [
     {header: 'Student Name', field: `firstName`, sortable: true},
@@ -92,7 +137,36 @@ export class StudentList {
     },
   ];
 
-  private mtxDialog = inject(MtxDialog);
+
+  ngOnInit(): void {
+    this.editStudentForm = this.formBuilder.group({
+      studentId: 0,
+      firstName: ['', [Validators.required, Validators.name]],
+      lastName: ['', [Validators.required, Validators.name]],
+      dob: [null],
+      class: '',
+      score: [0, [Validators.min(0), Validators.max(100)]],
+      passportPhotoUrl: '',
+      frontIDPhotoUrl: '',
+      backIDPhotoUrl: ''
+    });
+
+    this.editStudentForm.valueChanges.subscribe( (value: StudentData) => {
+      console.log(value);
+    });
+  }
+
+  get firstName(){
+    return this.editStudentForm.get('firstName');
+  }
+
+  get lastName(){
+    return this.editStudentForm.get('lastName');
+  }
+
+  get score(){
+    return this.editStudentForm.get('score');
+  }
 
   onAddStudent(){
     console.log('adding student');
@@ -133,6 +207,7 @@ export class StudentList {
   saveEditedStudentData(dialogRef: DialogRef)
   {
     console.log(`Saving edited student data`);
+    console.log(this.editStudentForm.value);
     dialogRef.close();
   }
 
@@ -147,5 +222,75 @@ export class StudentList {
   {
     console.log(`Confirmed delete of: ${this.studentDataBeingEdited?.firstName}`);
     dialogRef.close();
+  }
+
+
+  onPassportPhotoSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+
+    if( input.files && input.files.length > 0 )
+    {
+      this.selectedPassportPhoto = input.files[0];
+      
+        if( !this.selectedPassportPhoto?.type.startsWith('image/') )
+      {
+        alert('only images are allowed!');
+
+        this.selectedPassportPhoto = null;
+        this.previewPassportPhotoUrl = null;
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => this.previewPassportPhotoUrl = reader.result;
+
+      reader.readAsDataURL(this.selectedPassportPhoto);
+    }
+  }
+
+  onFrontIDPhotoSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+
+    if( input.files && input.files.length > 0 )
+    {
+      this.selectedFrontIDPhoto = input.files[0];
+      
+        if( !this.selectedFrontIDPhoto?.type.startsWith('image/') )
+      {
+        alert('only images are allowed!');
+
+        this.selectedFrontIDPhoto = null;
+        this.previewFrontIDPhotoUrl = null;
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => this.previewFrontIDPhotoUrl = reader.result;
+
+      reader.readAsDataURL(this.selectedFrontIDPhoto);
+    }
+  }
+
+  onBackIDPhotoSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+
+    if( input.files && input.files.length > 0 )
+    {
+      this.selectedBackIDPhoto = input.files[0];
+      
+        if( !this.selectedBackIDPhoto?.type.startsWith('image/') )
+      {
+        alert('only images are allowed!');
+
+        this.selectedBackIDPhoto = null;
+        this.previewBackIDPhotoUrl = null;
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => this.previewBackIDPhotoUrl = reader.result;
+
+      reader.readAsDataURL(this.selectedBackIDPhoto);
+    }
   }
 }
